7bbfc5ea829c41cd62aabe9534b4ac28b8d7d150
Jordan Taylor <jordan22taylor@gmail.com>
2021-10-26 01:39:35 -0600

    checkpoint reached

---
+++ project_4/js/main.js
@@ -0,0 +1,45 @@
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() + (max + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
+
+function getMonthName(month) {
+    let name
+    switch (month) {
+        case 1:
+            name = "January"
+            break
+            case 2:
+                name = "February"
+                break
+                default:
+                    name = "Not a month"
+                    break
+    }
+    return name
+}
+
+function getFortune(fate) {
+    let message
+    switch (fate) {
+        case 1:
+            message: 'be featured as the strongest superhero in the next Marvel movie'
+            break
+            case 2:
+                message: 'win millions of dollars from a long lost uncle in Nigeria'
+                break
+                default:
+                    message: 'Some sort of error occured'
+                    break
+    }
+}
+
+let fate = getRandomIntInclusive(1, 5)
+let day = getRandomIntInclusive(1, 30)
+let month = getRandomIntInclusive(1, 12)
+const monthName = getMonthName(month)
+
+const fortuneRevealed = 'On $(monthName} ${day}, you will ${fate}'
+
+document.querySelector('#fortune').innerText = fortuneRevealed
